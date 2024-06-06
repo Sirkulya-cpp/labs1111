bf87a29690bc4eb51fcae726b2a8a4c3dbce1e87
Sirkulya-cpp <dimasirko1112@gmail.com>
2024-06-06 11:51:47 +0300

    Reworked to signle app docker lab

---
+++ link_app.py
@@ -0,0 +1,22 @@
+import validators
+
+from flask import Flask, jsonify, redirect, render_template, request
+from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
+app = Flask(__name__)
+
+
+cors = CORS(app)
+app.config["CORS_HEADERS"] = "Content-Type"
+
+# вхід на сервіс
+@app.route("/")
+def unauth():
+    return render_template("index.html", template_folder="templates")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
